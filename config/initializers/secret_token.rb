4a6cad55a7394edf525f1b7939f72b88f755421c
huawei weng <hweng@ucsd.edu>
2012-12-20 15:42:26 -0800

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blacklight::Application.config.secret_token = '2c7da8de64f127be8e8381c801dda10acdbc070a59362ba5d0dae76efc7e4c806981eb96cbb32830b174ac138d61e944bde7a891953ee42f05aaefd9ce274e6b'
