6b98bfa249b3c74b99536d5770ca14b35fb7263c
huawei weng <hweng@ucsd.edu>
2013-01-03 15:39:35 -0800

    add content to faq and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Blacklight::Application.routes.draw do
-  get "static_pages/about"
-
-  get "static_pages/faq"
+  
+  match '/faq', to: 'static_pages#faq'
+  match '/about', to: 'static_pages#about'
 
   root :to => "catalog#index"
 
