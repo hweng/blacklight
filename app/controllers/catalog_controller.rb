166506dece75497c7eb41e4900f3b008ae8bd712
huawei weng <hweng@ucsd.edu>
2013-01-09 11:19:07 -0800

    added catalog, updated home text

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -55,7 +55,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
     config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
     config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
+    #config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
     config.add_facet_field 'subject_geo_facet', :label => 'Region' 
     config.add_facet_field 'subject_era_facet', :label => 'Era'  
 
