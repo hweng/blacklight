b6ca545ee733300c969034a17737ef30a2e6c876
Yoon Chang <yschang512@gmail.com>
2013-07-18 12:32:50 -0700

    edited new.html.erb and show.html.erb

--- tmp/dams_provenance_collections_controller.rb
+++ tmp/dams_provenance_collections_controller.rb
@@ -22,7 +22,14 @@ class DamsProvenanceCollectionsController < ApplicationController
     @mads_complex_subjects = MadsComplexSubject.all( :order=>"system_create_dtsi asc" )
     @dams_units = DamsUnit.all( :order=>"system_create_dtsi asc" )
     @dams_assembled_collections = DamsAssembledCollection.all( :order=>"system_create_dtsi asc" )
-    @dams_objects = DamsObject.all( :order=>"system_create_dtsi asc" )
+    # @dams_objects = DamsObject.all( :order=>"system_create_dtsi asc" )
+
+    @dams_provenance_collection = DamsProvenanceCollection.find(params[:id])
+    @dams_object = DamsObject.find(:all)
+    @object_id = @dams_provenance_collection.damsObjectURI.to_s.gsub /.*\//, ""
+    @object_name = @dams_object.find_all{|v| v.pid == @object_id}[0].name.first   
+
+
     @mads_languages = MadsLanguage.all( :order=>"system_create_dtsi asc" )
     @dams_provenance_collection_parts=DamsProvenanceCollectionPart.all( :order=>"system_create_dtsi asc" )
   end
